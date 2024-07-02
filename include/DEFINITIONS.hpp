d4e500c5cc953f913aa99ec0e083b011f41be0dc
O-rensa <131181504+O-rensa@users.noreply.github.com>
2024-07-02 23:35:52 +0800

    Made small updates

--- include/DEFINITIONS.hpp
+++ include/DEFINITIONS.hpp
@@ -1,6 +1,6 @@
 #pragma once 
 
-#define SCREEN_WIDTH 576
-#define SCREEN_HEIGHT 852
+#define SCREEN_WIDTH 384
+#define SCREEN_HEIGHT 568
 #define SPLASH_STATE_SHOW_TIME 3
 #define SPLASH_SCENE_BACKGROUND_FILEPATH "Resources/res/Splash Background.png"
